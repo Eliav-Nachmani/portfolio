c1893b20033bf3ab20c7bdc0308119b79db1d531
Eliav Nachmani <eliav.nachmani@saggiotechnologies.com>
2025-03-21 17:43:30 -0700

    Mobile Optimized 7

--- src/components/layout/ClientWrapper.tsx
+++ src/components/layout/ClientWrapper.tsx
@@ -9,7 +9,7 @@ export default function ClientWrapper({ children }: { children: React.ReactNode 
   const pathname = usePathname();
   const [showNoise, setShowNoise] = useState(false);
   const [currentPage, setCurrentPage] = useState(children);
-
+  const { angle } = useKnob(); // ✅ Read Knob state (DON'T reset it)
 
   useEffect(() => {
     setShowNoise(true); // Show static effect
